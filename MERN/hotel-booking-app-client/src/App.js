a3b56644b03779b31414c424b734608721ea1a85
Akhilesh Shende <akhilesh.x.shende@gsk.com>
2025-07-06 18:16:46 +0530

    hotel booking

---
+++ MERN/hotel-booking-app-client/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TopNav from "./components/TopNav";
+import Home from "./booking/Home";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <TopNav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
