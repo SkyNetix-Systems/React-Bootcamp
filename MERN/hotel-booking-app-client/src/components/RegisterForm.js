56bcb69938a6229fadd084c009e37e380bab8c0a
Akhilesh Shende <akhilesh.x.shende@gsk.com>
2025-07-07 13:54:11 +0530

    added register form

---
+++ MERN/hotel-booking-app-client/src/components/RegisterForm.js
@@ -0,0 +1,52 @@
+const RegisterForm = ({
+    handleSubmit,
+    name,
+    setName,
+    email,
+    setEmail,
+    password,
+    setPassword,
+}) => (
+    <form onSubmit={handleSubmit} className="mt-3">
+        <div className="form-group mb-3">
+            <label className="form-label">Your Name</label>
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Enter name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+        </div>
+
+        <div className="form-group mb-3">
+            <label className="form-label">Email Address</label>
+            <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+        </div>
+
+        <div className="form-group mb-3">
+            <label className="form-label">Password</label>
+            <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+        </div>
+
+        <div className="text-center">
+            <button type="submit" className="btn btn-primary">
+                Register
+            </button>
+        </div>
+    </form>
+);
+
+export default RegisterForm;
