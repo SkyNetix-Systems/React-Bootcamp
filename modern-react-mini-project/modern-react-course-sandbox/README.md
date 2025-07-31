2b9ee1bc28e43fb16a3985f5dca5ef8e3225ac18
Akhilesh Shende <akhilesh@skynetixsystems.com>
2025-07-31 19:48:08 +0530

    refactoring

---
+++ modern-react-mini-project/modern-react-course-sandbox/README.md
@@ -0,0 +1,3 @@
+# Modern React Course Mini-Projects
+
+This is the code for some of the smaller projects from the Modern React From The Beginning course that we used to teach certain concepts.
---
+++ modern-react-mini-project/notes-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-green-500">
+        Tailwind is working 🎉
+      </h1>
+    </div>
+  );
+};
+
+export default App;
