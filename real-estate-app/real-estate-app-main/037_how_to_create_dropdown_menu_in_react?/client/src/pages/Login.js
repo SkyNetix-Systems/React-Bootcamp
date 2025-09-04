fea5eb4ffc3d7a558e948e1bdb385d732d35162a
Akhilesh Shende <akhilesh@skynetixsystems.com>
2025-09-04 13:20:21 +0000

    realestateapp

--- real-estate-app/real-estate-app-main/037_how_to_create_dropdown_menu_in_react?/client/src/pages/Login.js
+++
@@ -1,7 +0,0 @@
-export default function Login() {
-  return (
-    <div>
-      <h1 className="display-1 bg-primary text-light p-5">Login</h1>
-    </div>
-  );
-}
--- real-estate-app/real-estate-app-main/037_how_to_create_dropdown_menu_in_react?/client/src/pages/Register.js
+++
@@ -1,7 +0,0 @@
-export default function Register() {
-  return (
-    <div>
-      <h1 className="display-1 bg-primary text-light p-5">Register</h1>
-    </div>
-  );
-}
