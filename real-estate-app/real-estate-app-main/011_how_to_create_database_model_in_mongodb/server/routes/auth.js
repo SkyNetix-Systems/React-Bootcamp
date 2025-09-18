08fef798a4c0f0ee887c1c4625dfd55edd150805
Akhilesh Shende <akhilesh@skynetixsystems.com>
2025-09-18 13:47:06 +0530

    food app

--- real-estate-app/real-estate-app-main/011_how_to_create_database_model_in_mongodb/server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-import * as auth from "../controllers/auth.js";
-
-router.get("/", auth.welcome);
-
-export default router;
--- real-estate-app/real-estate-app-main/097_how_to_use_like_unlike_feature_in_react/client/src/config.js
+++
@@ -1,2 +0,0 @@
-export const API = "http://localhost:8000/api";
-export const GOOGLE_PLACES_KEY = "AIzaSyDeaCvuV2krPlpVWEzGcQ2cWwmc5s5k4NU";
